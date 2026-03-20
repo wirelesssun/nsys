d2f5e96054c10ad731fd0bdbeb074fe7617b6951
your_email <your_email@example.com>
2026-03-20 10:29:33 +0800

    ADD Practise_04_LIB_MUL.

---
+++ Practise_04_LIB_MUL/AppProjectB/build/CMakeFiles/CalculatorApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalculatorApp.dir/src/main.cpp.o"
+  "CMakeFiles/CalculatorApp.dir/src/main.cpp.o.d"
+  "CalculatorApp"
+  "CalculatorApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalculatorApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
